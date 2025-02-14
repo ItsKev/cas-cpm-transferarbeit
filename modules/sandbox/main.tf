86be495543487b0f9e7e4e12589ccff6abd4207d
ItsKev <kevin.huber@hotmail.com>
2025-02-14 12:58:05 +0100

    Add Azure policies

--- modules/sandbox/main.tf
+++ modules/sandbox/main.tf
@@ -2,3 +2,11 @@ resource "azurerm_resource_group" "sandbox" {
   name     = var.resource_group_name
   location = "East US"
 }
+
+resource "azurerm_resource_group_policy_assignment" "policies" {
+  for_each = { for policy in var.policy_assignments : policy.name => policy }
+
+  name                 = "${each.value.name}-assignment-${azurerm_resource_group.sandbox.name}"
+  resource_group_id    = azurerm_resource_group.sandbox.id
+  policy_definition_id = each.value.id
+}
--- modules/sandbox/variables.tf
+++ modules/sandbox/variables.tf
@@ -1,3 +1,11 @@
 variable "resource_group_name" {
   type = string
 }
+
+variable "policy_assignments" {
+  description = "List of policy definitions to assign"
+  type = list(object({
+    name = string
+    id   = string
+  }))
+}
